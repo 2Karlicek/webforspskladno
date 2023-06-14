1c0b2851460dd9b90d32c51335250e8d0d7047e4
Kamcatka699 <136564173+Kamcatka699@users.noreply.github.com>
2023-06-14 14:52:58 +0200

    Update bartos.php

--- bartos.php
+++ bartos.php
@@ -25,17 +25,19 @@ $_SESSION['previous_page'] = $_SERVER['REQUEST_URI'];
 
 	<div class="main">
 	<div class="row">
-		<div class="column"><a href="https://cs-cz.facebook.com/tomio.cz/" target="_blank"><img  src="https://1000logos.net/wp-content/uploads/2021/04/Facebook-logo.png"></a></div>
-		<div class="column"><a href="https://twitter.com/tomio_cz" target="_blank"><img  src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png" ></a></div>
-		<div class="column"><a href="https://www.youtube.com/channel/UCe0c2rUjCHtQSf2XY7ulCQA" target="_blank"><img  src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/2560px-YouTube_full-color_icon_%282017%29.svg.png" ></a></div>
+		<div class="column"><a href="https://www.facebook.com/PiratIvanBartos/" target="_blank"><img  src="https://1000logos.net/wp-content/uploads/2021/04/Facebook-logo.png"></a></div>
+		<div class="column"><a href="https://twitter.com/PiratIvanBartos" target="_blank"><img  src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png" ></a></div>
+		<div class="column"><a href="https://www.youtube.com/channel/UC_zxYLGrkmrYazYt0MzyVlA" target="_blank"><img  src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/2560px-YouTube_full-color_icon_%282017%29.svg.png" ></a></div>
 	</div>
-		<p>arodil se v Tokiu, kam se jeho česká matka Helena Okamurová, rozená Holíková, po svatbě s občanem
-		Japonska japonsko-korejského původu Matsuem Okamurou v roce 1966 přestěhovala. V Japonsku však 
-		Tomio Okamura strávil necelých 10 let. Většinu života prožil v České republice
-		(respektive v Československu).[7] Jeho matka se nesmířila s osamělým životem v 
-		Japonsku a vrátila se se svými syny do Československa, když mu bylo</p>
+		<p> Je český politik, od prosince 2021 místopředseda vlády Petra Fialy pro digitalizaci a ministr pro místní rozvoj ČR, v letech 2009 až 2014 a opět od roku 2016 předseda České pirátské strany, od října 2017 poslanec Poslanecké sněmovny PČR.
+Pirátskou stranu sám označuje jako demokratickou politickou platformu, která se pokouší reflektovat realitu třetího tisíciletí. „Technologie, které máme nyní k dispozici, nám mohou úžasně pomáhat a ve finále všem lidem ulehčit život. Nesmí se ovšem stát nástrojem digitální totality.
+Považuje se za pacifistu. V roce 2017 vyjádřil kritiku některých intervencí NATO. V roce 2019 ocenil práci vojáků a vyjádřil nezbytnost modernizovat armádu v koordinaci se spojenci z NATO a EU. 
+Ivan Bartoš byl pražským členem SPD Janem Čížkem nepravdivě označen za současného nebo bývalého člena organizace Antifa, zároveň Piráty nařknul z ničení billboardů. Piráti naopak podali trestní oznámení na SPD, která zveřejňovala vymyšlené fiktivní příspěvky na sociálních sítích v neprospěch Pirátů Podle Tomia Okamury, Bartoš s krajně levicovou organizací Antifa sympatizuje. Bartoš vazby na Antifu popřel a vlajku Antifaschistische Aktion, se kterou je vyfocen na několika dostupných fotografiích, označuje za připomínku protinacistického odporu ve 30. letech 20. století. Předseda Pirátů na toto téma uvedl, že být proti fašismu považuje za normální a vymezil se proti radikalismu Tomia Okamury: "V momentě, kdy pan Konvička, lidé z Hnutí Úsvit, pan Okamura, antisemita Adam B. Bartoš a další výtečníci české rádoby národovecké vlny v Praze exhibovali do té míry, že na náměstích stavěli šibenice, cítil jsem jako svou morální povinnost, říci tomuto způsobu prezentace jasné ne." 
+V lednu 2014 obhájil na Celostátním fóru Pirátů v Pardubicích post předsedy České pirátské strany, když pro něj hlasovalo 89 ze 116 delegátů (tj. 77 %).
+Ve volbách do Evropského parlamentu v roce 2014 kandidoval jako lídr Pirátů. Piráti získali jen 4,78 % hlasů a do EP se nedostali. Bartoš však získal 12 644 preferenčních hlasů, což byl v ČR 13. nejvyšší počet. Dva týdny po volbách oznámil rezignaci na předsednickou funkci ve straně, kterou zdůvodnil únavou po volební kampani, ideovým rozdělením strany a potřebě věnovat se víc soukromému a rodinnému životu.
+</p>
 
-		<img src="https://tomio.cz/wp-content/uploads/2020/02/Tomio_Okamura_8-1-e1588927265909.jpg">
+		<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Ivan_Barto%C5%A1_16_December_2021.jpg/225px-Ivan_Barto%C5%A1_16_December_2021.jpg">
 	</div>
 <?php
 
